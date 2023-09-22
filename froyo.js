a8cb165f6cfa79cd60062c623d98c774ca82599b
Home PC <vfxprojectguy@gmail.com>
2023-09-22 08:16:16 -0700

    add function

--- froyo.js
+++ froyo.js
@@ -1,4 +1,4 @@
-const userObjectText = prompt('Please enter a list of comma-separated froyo flavors.');
+const userObjectText = prompt('Please enter a list of comma-separated froyo flavors.','vanilla,vanilla,vanilla,strawberry,coffee,coffee');
 
 
 const userObjectArray = userObjectText.split(',');
@@ -6,17 +6,34 @@ const userObjectArray = userObjectText.split(',');
 
 console.log(userObjectArray)
 
+//function starts here
+const countFlavors = (arr) => {
 
-const flavorcounter = {};
+    //object to hold the flavors and counts
+    const flavorcounter = {};
 
-for (let i = 0; i < userObjectArray.length; i++) {
-    const flavor = userObjectArray[i].trim();
-    
-    if(!flavorcounter[flavor]){ 
-        flavorcounter[flavor] = 0
-    } 
-       flavorcounter[flavor] += 1;
+    //for loop iterate through arr
+    for (let i = 0; i < userObjectArray.length; i++) {
+        const flavor = userObjectArray[i].trim();
+
+        //if there is no value 
+        if(!flavorcounter[flavor]){ 
+            //define the value = 0
+            flavorcounter[flavor] = 0
+        } 
+        //add value +1 
+           flavorcounter[flavor] += 1;
+    }
+    //return object
+return flavorcounter;
 }
 
 
-console.table(flavorcounter);
+const runFunc = countFlavors(userObjectArray)
+
+
+
+console.table(runFunc);
+
+
+
